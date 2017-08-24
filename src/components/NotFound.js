bce3645d00445d1c9e09d026ec9f41cd87a87743
jbenitez <v-jbenitez@hotwire.com>
2017-08-24 01:49:47 -0500

    Router Component Added

---
+++ src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// This is a good example of a Stateless Functional Component.
+const NotFound = (props) => {
+  return (
+    <header className="top">
+      <h1>Not Found</h1>
+    </header>
+  )
+}
+
+export default NotFound;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Miss, Match } from 'react-router';
+
+//Import Styles
 import './css/style.css';
+
+//Import Components
 import App from './components/App';
 import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 
-render(<App/>, document.getElementById('main'));
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker} />
+        <Match pattern="/store/:storeId" component={App} />
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+render(<Root/>, document.getElementById('main'));
